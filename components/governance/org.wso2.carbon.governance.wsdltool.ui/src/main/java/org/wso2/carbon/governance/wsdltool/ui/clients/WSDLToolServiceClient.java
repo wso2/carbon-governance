83d326831fef7bf1c6798819733e13a88dfeb4ac
Isuruwan Herath <isuruwan@gmail.com>
2015-03-10 16:56:15 +0530

    wsdl diff tool impl

--- components/governance/org.wso2.carbon.governance.wsdltool.ui/src/main/java/org/wso2/carbon/governance/wsdltool/ui/clients/WSDLToolServiceClient.java
+++ components/governance/org.wso2.carbon.governance.wsdltool.ui/src/main/java/org/wso2/carbon/governance/wsdltool/ui/clients/WSDLToolServiceClient.java
@@ -23,6 +23,7 @@ import org.apache.axis2.context.ConfigurationContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.CarbonConstants;
+import org.wso2.carbon.governance.wsdltool.stub.WSDLToolServiceExceptionException;
 import org.wso2.carbon.governance.wsdltool.stub.WSDLToolServiceStub;
 import org.wso2.carbon.governance.wsdltool.stub.beans.xsd.ServiceInfoBean;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
@@ -31,6 +32,7 @@ import org.wso2.carbon.utils.ServerConstants;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpSession;
+import java.rmi.RemoteException;
 
 public class WSDLToolServiceClient {
 
@@ -39,20 +41,15 @@ public class WSDLToolServiceClient {
     private WSDLToolServiceStub stub;
     private String epr;
 
-    public WSDLToolServiceClient(
-            String cookie, String backendServerURL, ConfigurationContext configContext)
+    public WSDLToolServiceClient(ConfigurationContext configContext, String backendServerURL, String cookie)
             throws RegistryException {
-
         epr = backendServerURL + "WSDLToolService";
-
         try {
             stub = new WSDLToolServiceStub(configContext, epr);
-
             ServiceClient client = stub._getServiceClient();
             Options option = client.getOptions();
             option.setManageSession(true);
             option.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, cookie);
-
         } catch (AxisFault axisFault) {
             String msg = "Failed to initiate wsdltool service client. " + axisFault.getMessage();
             log.error(msg, axisFault);
@@ -86,5 +83,48 @@ public class WSDLToolServiceClient {
 
     public void addMEXService(String path, ServiceInfoBean serviceInfo) throws Exception {
         stub.addMEXService(path, serviceInfo);
+    }
+
+    /**
+     * Method for get membrane diff
+     * @param resource1Path registry path of resource1
+     * @param resource2Path registry path of resource2
+     * @param type result type
+     * @return String[] membrane result diff array
+     * @throws RegistryException
+     */
+    public String[] getMembraneDiffArrayResult(String resource1Path, String resource2Path, String type)
+            throws RegistryException {
+        try {
+            // Get membrane diff array
+            return stub.getMembraneDiffArrayResult(resource1Path, resource2Path, type);
+        } catch (RemoteException e) {
+            String msg = "Backend Service is unavailable";
+            throw new RegistryException(msg, e);
+        } catch (WSDLToolServiceExceptionException e) {
+            String msg = "Cannot get the Membrane descriptive diff";
+            throw new RegistryException(msg, e);
+        }
+    }
+
+    /**
+     * Method to get diff view type
+     * @param resourcePath1 registry path of resource1
+     * @param resourcePath2 registry path of resource2
+     * @return String diff type string
+     * @throws RegistryException
+     */
+    public String getDiffViewType(String resourcePath1, String resourcePath2)
+            throws RegistryException {
+        try {
+            // Get type of the diff view
+            return stub.getDiffViewType(resourcePath1, resourcePath2);
+        } catch (RemoteException e) {
+            String msg = "Backend Service is unavailable";
+            throw new RegistryException(msg, e);
+        } catch (Exception e) {
+            String msg = "Backend Service is unavailable";
+            throw new RegistryException(msg, e);
+        }
     }
 }
