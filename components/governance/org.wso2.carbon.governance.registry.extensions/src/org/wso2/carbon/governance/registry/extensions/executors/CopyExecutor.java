5b7f33cf7b2c0c743c400ad2cc552f06cdecc2d5
daneshk <dknkuruppu@gmail.com>
2015-10-22 16:15:55 +0530

    Remove setting new resource to the requestCOntext

--- components/governance/org.wso2.carbon.governance.registry.extensions/src/org/wso2/carbon/governance/registry/extensions/executors/CopyExecutor.java
+++ components/governance/org.wso2.carbon.governance.registry.extensions/src/org/wso2/carbon/governance/registry/extensions/executors/CopyExecutor.java
@@ -102,9 +102,6 @@ public class CopyExecutor implements Execution {
                 addNewId(requestContext.getRegistry(), newResource, newPath);
             }
 
-            requestContext.setResource(newResource);
-            requestContext.setResourcePath(new ResourcePath(newPath));
-
 //            Copying comments
             copyComments(requestContext.getRegistry(), newPath, resourcePath);
 
