f1a9d416bfba9d5ac92cbb40230e5fa7a6976a59
lahirue <lahirue@wso2.com>
2016-09-02 11:19:25 +0530

    solving taxonomy pom version ranges and update throw clause

--- components/governance/org.wso2.carbon.governance.taxonomy.ui/src/main/java/org/wso2/carbon/governance/taxonomy/ui/clients/TaxonomyManagementClient.java
+++ components/governance/org.wso2.carbon.governance.taxonomy.ui/src/main/java/org/wso2/carbon/governance/taxonomy/ui/clients/TaxonomyManagementClient.java
@@ -32,6 +32,7 @@ import javax.servlet.http.HttpSession;
  * taxonomy stub
  */
 public class TaxonomyManagementClient {
+    public static final String TAXONOMY_SERVICES = "TaxonomyServices";
     private TaxonomyServicesStub stub;
 
     public TaxonomyManagementClient(String cookie, ServletConfig config, HttpSession session)
@@ -40,7 +41,7 @@ public class TaxonomyManagementClient {
         String backendServerURL = CarbonUIUtil.getServerURL(config.getServletContext(), session);
         ConfigurationContext configContext = (ConfigurationContext) config.
                 getServletContext().getAttribute(CarbonConstants.CONFIGURATION_CONTEXT);
-        endpointURL = backendServerURL + "TaxonomyServices";
+        endpointURL = backendServerURL + TAXONOMY_SERVICES;
 
         try {
             stub = new TaxonomyServicesStub(configContext, endpointURL);
