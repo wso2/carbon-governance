29d4a7bcf1428a3f748544bc3eb553cb0d7a0773
Thushara Kasun Ranawaka <thushara@Thusharas-MacBook-Pro.local>
2017-02-03 22:17:56 +0800

    Fix for REGISTRY-2993

---
+++ components/governance/org.wso2.carbon.governance.rest.api/src/main/java/org/wso2/carbon/governance/rest/api/swagger/SwaggerJaxrsConfig.java
@@ -0,0 +1,27 @@
+package org.wso2.carbon.governance.rest.api.swagger;
+
+import com.wordnik.swagger.jaxrs.config.BeanConfig;
+import org.wso2.carbon.utils.NetworkUtils;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "SwaggerJaxrsConfig", loadOnStartup = 1)
+public class SwaggerJaxrsConfig extends BeanConfig {
+
+    public SwaggerJaxrsConfig(){
+        super();
+    }
+
+    public void setBasePath(String basePath)
+    {
+        // Hostname
+        String hostName = "localhost";
+        try {
+            hostName = NetworkUtils.getMgtHostName();
+        } catch (Exception ignored) {
+        }
+
+        super.setBasePath("https://" +
+                hostName + ":" + System.getProperty("mgt.transport.https.port") + "/governance");
+    }
+}
