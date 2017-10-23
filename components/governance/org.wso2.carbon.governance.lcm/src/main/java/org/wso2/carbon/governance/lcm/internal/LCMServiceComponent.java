b175de9e7a287e202deed974d6546ee366dfbec8
cnapagoda <cnapagoda@gmail.com>
2017-10-23 18:02:41 +0530

    fix formatting issue

--- components/governance/org.wso2.carbon.governance.lcm/src/main/java/org/wso2/carbon/governance/lcm/internal/LCMServiceComponent.java
+++ components/governance/org.wso2.carbon.governance.lcm/src/main/java/org/wso2/carbon/governance/lcm/internal/LCMServiceComponent.java
@@ -26,9 +26,13 @@ import org.wso2.carbon.governance.lcm.listener.LifecycleLoader;
 import org.wso2.carbon.governance.lcm.services.LifeCycleService;
 import org.wso2.carbon.governance.lcm.util.CommonUtil;
 import org.wso2.carbon.registry.common.AttributeSearchService;
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
 import org.wso2.carbon.registry.core.service.RegistryService;
 import org.wso2.carbon.utils.Axis2ConfigurationContextObserver;
 import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.FileNotFoundException;
 
 /**
  * @scr.component name="org.wso2.carbon.governance.lcm"
@@ -51,8 +55,8 @@ public class LCMServiceComponent {
         	PrivilegedCarbonContext.startTenantFlow();
         	PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(-1234, true);
         	PrivilegedCarbonContext.getThreadLocalCarbonContext().setUsername(CarbonConstants.REGISTRY_SYSTEM_USERNAME);
-            RegistryService registryService = LifeCycleServiceHolder.getInstance().getRegistryService();
-            CommonUtil.addDefaultLifecyclesIfNotAvailable(registryService.getConfigSystemRegistry(),
+        	RegistryService registryService = LifeCycleServiceHolder.getInstance().getRegistryService();
+        	CommonUtil.addDefaultLifecyclesIfNotAvailable(registryService.getConfigSystemRegistry(),
                     registryService.getRegistry(CarbonConstants.REGISTRY_SYSTEM_USERNAME));
         } catch (XMLStreamException | FileNotFoundException | RegistryException e) {
             log.error("An error occurred while setting up Governance Life Cycle Management", e);
